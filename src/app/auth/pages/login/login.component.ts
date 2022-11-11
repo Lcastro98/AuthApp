5b58b6bc96d33f162db810df2ab5d5c50c3bdbc6
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-11-11 16:22:18 -0500

    Mensajes de error visuales

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -25,10 +26,10 @@ export class LoginComponent {
 
     this.authService.login(email, password)
       .subscribe(ok => {
-        if(ok) {
+        if(ok === true) {
           this.router.navigateByUrl('/dashboard');
         } else {
-          //TODO: Mostrar mensaje de error
+          Swal.fire('Error', ok, 'error');
         }
       })
   }
