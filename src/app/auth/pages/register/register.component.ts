c6885c65f6395c07de6ac8a27a78ca67a79df95b
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-11-13 10:03:57 -0500

    Registro

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -17,11 +19,19 @@ export class RegisterComponent {
   })
 
   constructor(private fb: FormBuilder,
-              private router: Router) { }
+              private router: Router,
+              private authService: AuthService) { }
 
   register() {
-    console.log(this.miFormulario.value);
+    const {name, email, password} = this.miFormulario.value;
 
-    this.router.navigateByUrl('/dashboard');
+    this.authService.registro(name, email, password)
+      .subscribe(ok => {
+        if(ok === true) {
+          this.router.navigateByUrl('/dashboard');
+        } else {
+          Swal.fire('Error', ok, 'error');
+        }
+      })
   }
 }
